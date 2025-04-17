5b331fc47b03f1063fcfb3692f39585be8a0144c
余味 <yuwei1228@hotmail.com>
2025-04-17 23:18:17 +0800

    fix(deploy): 修正容器健康检查逻辑
    - 调整健康状态检查顺序，确保 'starting' 状态的检查在前- 添加 'healthy' 状态的检查，以正确设置 all_healthy 标志
    - 优化健康状态的打印逻辑，提高日志可读性

--- deploy.py
+++ deploy.py
@@ -167,12 +167,14 @@ def check_container_health(url, jwt, endpoint_id, stack_name, timeout=300):
                 continue
 
             health_status = health.get('Status')
+            print(f"容器 {container_id} 健康状态：{health_status}")
+
             if health_status == 'starting':
-                print(f"容器 {container_id} 健康状态：{health_status}")
                 all_healthy = False
             if health_status == 'unhealthy':
-                print(f"容器 {container_id} 健康状态：{health_status}")
                 return False
+            if health_status == 'healthy':
+                all_healthy = True
 
         if all_healthy:
             print("所有容器均健康")
